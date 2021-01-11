52aa006c40494605241067bf74382a2d4369029d
Martin Zemek <martin.zemek@qoretechnologies.com>
2021-01-11 14:47:34 +0100

    alpine docker tests

---
+++ examples/test/docker_test/test-alpine.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+set -x
+
+ENV_FILE=/tmp/env.sh
+
+# setup QORE_SRC_DIR env var
+cwd=`pwd`
+if [ "${QORE_SRC_DIR}" = "" ]; then
+    if [ -e "$cwd/qlib/SqlUtil.qm" ] || [ -e "$cwd/bin/qdbg" ] || [ -e "$cwd/cmake/QoreMacros.cmake" ] || [ -e "$cwd/lib/QoreLib.cpp" ]; then
+        QORE_SRC_DIR=$cwd
+    else
+        QORE_SRC_DIR=$WORKDIR/qore
+    fi
+fi
+
+echo "export QORE_SRC_DIR=${QORE_SRC_DIR}" >> ${ENV_FILE}
+
+echo "export QORE_UID=999" >> ${ENV_FILE}
+echo "export QORE_GID=999" >> ${ENV_FILE}
+
+. ${ENV_FILE}
+
+export MAKE_JOBS=6
+
+# build Qore and install
+echo && echo "-- building Qore --"
+cd ${QORE_SRC_DIR}
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=debug -DSINGLE_COMPILATION_UNIT=1 -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
+make -j${MAKE_JOBS}
+make install
+
+# add Qore user and group
+addgroup -g ${QORUS_GID} qore
+adduser -u ${QORUS_UID} -h ${OMQ_DIR} qore
+
+# own everything by the qore user
+chown -R qore:qore ${QORE_SRC_DIR}
+
+# run the tests
+export QORE_MODULE_DIR=${QORE_SRC_DIR}/qlib:${QORE_MODULE_DIR}
+cd ${QORE_SRC_DIR}
+gosu qore:qore ./run_tests.sh
